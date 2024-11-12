e418162ff07d9689d57fd931777fb7f723cedbfd
tuandanh <nguyentuandanhk15@siu.edu.vn>
2024-11-12 17:16:11 +0700

    first

---
+++ src/main/java/com/example/learningeveryday/LearningeverydayApplication.java
@@ -0,0 +1,13 @@
+package com.example.learningeveryday;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningeverydayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningeverydayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/learningeveryday/LearningeverydayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.learningeveryday;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningeverydayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
